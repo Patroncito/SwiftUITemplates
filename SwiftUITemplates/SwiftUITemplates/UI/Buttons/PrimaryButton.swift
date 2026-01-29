eab91253fc3da716d5af005638ea3da311726f6f
FERMIN ALFONSO BANUELOS PATRON <fermin.banuelos@elektra.com.mx>
2026-01-29 12:06:40 -0600

    nueva estructura de carpetas

---
+++ SwiftUITemplates/SwiftUITemplates/UI/Buttons/PrimaryButton.swift
@@ -0,0 +1,18 @@
+//
+//  PrimaryButton.swift
+//  SwiftUITemplates
+//
+//  Created by Alfonso Patron on 29/01/26.
+//
+
+import SwiftUI
+
+struct PrimaryButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PrimaryButton()
+}
