23ce7028246c907fc2d70227dc38e2335de4900d
FERMIN ALFONSO BANUELOS PATRON <fermin.banuelos@elektra.com.mx>
2026-01-28 18:40:54 -0600

    first commit

---
+++ SwiftUITemplates/SwiftUITemplates/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUITemplates
+//
+//  Created by Alfonso Patron on 28/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUITemplates/SwiftUITemplates/SwiftUITemplatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITemplatesApp.swift
+//  SwiftUITemplates
+//
+//  Created by Alfonso Patron on 28/01/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITemplatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
